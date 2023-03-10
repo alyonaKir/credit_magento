af5bde4d7e941d339b7acf5221e2a46a3b839d53
user <sehfu@wdv.com>
2023-03-10 13:17:26 +0300

    Added custom tab to customer account, add form to send apply for credit, add saving to db after sending form and simplify routes.xml

---
+++ app/code/AlyonaKir/Credit/Block/Form/ApplyForCredit.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace AlyonaKir\Credit\Block\Form;
+
+use Magento\Framework\View\Element\Template;
+
+class ApplyForCredit extends Template
+{
+}
---
+++ app/code/AlyonaKir/Credit/Controller/Customer/Form.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace AlyonaKir\Credit\Controller\Customer;
+
+use AlyonaKir\Credit\Model\Credit\CreditFactory;
+use AlyonaKir\Credit\Model\Credit\CreditRepositoryFactory;
+
+
+class Form extends \Magento\Framework\App\Action\Action
+{
+
+    protected $_pageFactory;
+    protected $creditFactory;
+
+    protected $creditRepositoryFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context      $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        CreditFactory                              $creditFactory,
+        CreditRepositoryFactory                    $creditRepositoryFactory
+    )
+    {
+        $this->creditFactory = $creditFactory;
+        $this->creditRepositoryFactory = $creditRepositoryFactory;
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $creditRepository = $this->creditRepositoryFactory->create();
+        if (isset($_POST['file']) && isset($_POST['credit_limit'])) {
+            $credit = $this->creditFactory->create();
+            try {
+                $creditdata = [
+                    'credit_limit' => $_POST['credit_limit'],
+                    'file' => $_POST['file']
+                ];
+                $credit->setData($creditdata);
+                $creditRepository->save($credit);
+            } catch (\Exception $ex) {
+                $this->messageManager->addErrorMessage("Something went wrong");
+            }
+            $this->messageManager->addSuccessMessage("The request will be processed in three days.");
+        }
+
+        return $this->_pageFactory->create();
+    }
+}
+
